d8af2ca6d6463be35a2566f94657be85e783b3c6
bigcatplichta <bigcatplichta@gmail.com>
2020-06-11 21:44:29 -0600

    edit footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,17 +4,16 @@ import './Footer.css'
 
 export default () => (
   <div>
-    <h2 className="taCenter">
+    {/* <h2 className="taCenter">
       Follow us{' '}
       <a href="https://instagram.com/thrivegoldcoast/">@thrivegoldcoast</a>
-    </h2>
+    </h2> */}
     <br />
-    <InstagramFeed count="8" />
+    {/* <InstagramFeed count="8" /> */}
     <footer className="footer">
       <div className="container taCenter">
         <span>
-          © Copyright {new Date().getFullYear()} All rights reserved. Crafted by{' '}
-          <a href="https://thriveweb.com.au/">Thrive</a>.
+          © Copyright {new Date().getFullYear()}
         </span>
       </div>
     </footer>
