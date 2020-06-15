bc6088c47f3dcfb56b58454b666ea5b210b1fd64
bigcatplichta <bigcatplichta@gmail.com>
2020-06-15 16:18:37 -0600

    remove checkbox for newsletter

--- src/components/FormSimpleAjax.js
+++ src/components/FormSimpleAjax.js
@@ -150,14 +150,6 @@ class Form extends React.Component {
             />
             <span>Message</span>
           </label>
-          <label className="Form--Label Form-Checkbox">
-            <input
-              className="Form--Input Form--Textarea Form--CheckboxInput"
-              name="newsletter"
-              type="checkbox"
-            />
-            <span>Get news updates</span>
-          </label>
           <div
             className="g-recaptcha"
             data-sitekey="6LfKN3kUAAAAAGIM1CbXmaRZx3LIh_W2twn1tzkA"
