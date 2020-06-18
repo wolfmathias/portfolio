5d370716a7f694a381e962eb34102ecc1613507f
bigcatplichta <bigcatplichta@gmail.com>
2020-06-17 18:24:07 -0600

    add social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import InstagramFeed from './InstagramFeed'
 import './Footer.css'
+import {GitHub, Twitter, Mail} from 'react-feather'
 
 export default () => (
   <div>
@@ -11,6 +12,17 @@ export default () => (
     <br />
     {/* <InstagramFeed count="8" /> */}
     <footer className="footer">
+      <div className="container taCenter ">
+        <a className="footer-contact-item" href={`https://www.github.com/bigcatplichta`}>
+          <GitHub /> github.com/bigcatplichta
+        </a>
+        <a className="footer-contact-item" href={`https://www.twitter.com/mattplichtawild`}>
+          <Twitter /> @mattplichtawild
+        </a>
+        <a className="footer-contact-item" href={`mailto:mattplichtawild@gmail.com`}>
+          <Mail /> mattplichtawild@gmail.com
+        </a>
+      </div>
       <div className="container taCenter">
         <span>
           © Copyright {new Date().getFullYear()}
